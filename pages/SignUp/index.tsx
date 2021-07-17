dfe866a1ed81571e08835a49e920778af90d90de
hyoseon <dott.goo@gmail.com>
2021-07-17 22:37:32 +0900

    chore(signUp): a 태그 Link 태그로 변경

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react';
 import { Form, Label, Input, LinkContainer, Button, Header, Error, Success } from './style';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   const [email, onChangeEmail, setEmail] = useInput('');
@@ -113,7 +114,7 @@ const SignUp = () => {
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <a href="/login">로그인 하러가기</a>
+        <Link to="/login">로그인 하러가기</Link>
       </LinkContainer>
     </div>
   );
