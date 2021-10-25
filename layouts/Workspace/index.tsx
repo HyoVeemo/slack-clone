75952ffff7c4c5c5a4f4a40ed178493e5d14778c
hyoveemo <dott.goo@gmail.com>
2021-10-25 22:34:51 +0900

    chore(layouts): 프로필 메뉴 컴포넌트 적용

--- layouts/Workspace/index.tsx
+++ layouts/Workspace/index.tsx
@@ -2,14 +2,14 @@
 import {
   Channels,
   Chats,
-  Header, MenuScroll,
-  ProfileImg,
+  Header, LogOutButton, MenuScroll,
+  ProfileImg, ProfileModal,
   RightMenu,
   WorkspaceName,
   Workspaces,
   WorkspaceWrapper,
 } from '@layouts/Workspace/style';
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
@@ -24,6 +24,7 @@ const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 // children 을 사용하는 컴포넌트 FC (children 을 안 쓰는 컴포넌트는 VFC type)
 const Index: FC = ({ children }) => {
+  const [showUserMenu, setShowUserMenu] = useState(false)
   const { data, error, revalidate, mutate } = useSWR('http://localhost:3095/api/users', fetcher, { dedupingInterval: 100000 });
 
   const onLogout = useCallback(() => {
@@ -37,6 +38,11 @@ const Index: FC = ({ children }) => {
       });
   }, []);
 
+  // 보통 토글 함수라고 부른다.
+  const onClickUserProfile = useCallback(()=>{
+    setShowUserMenu((prev)=>!prev)
+  }, [])
+
   if (!data) {
     return <Redirect to="/login"></Redirect>;
   }
@@ -46,11 +52,19 @@ const Index: FC = ({ children }) => {
       <Header>
         slack
         <RightMenu>
-          <span>
-            <ProfileImg src={gravatar.url(data.nickname, {s:'28px', d:'retro'})} alt={data.nickname}>
-            </ProfileImg>
-            <Menu>프로필메뉴</Menu>
-          </span>
+          <span onClick={onClickUserProfile}>
+            <ProfileImg src={gravatar.url(data.nickname, {s:'28px', d:'retro'})} alt={data.nickname}/>
+               {showUserMenu && <Menu style={{right: 0, top:38}} show = {showUserMenu} onCloseModal={onClickUserProfile} >
+                  <ProfileModal>
+                    <img src={gravatar.url(data.nickname, {s:'36px', d: 'retro'})} alt={data.nickname}/>
+                    <div>
+                      <span id="profile-name">{data.nickname}</span>
+                      <span id="profile-active">Active</span>
+                    </div>
+                  </ProfileModal>
+                 <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+                 </Menu>}
+           </span>
         </RightMenu>
       </Header>
       <button onClick={onLogout}> 로그아웃</button>
