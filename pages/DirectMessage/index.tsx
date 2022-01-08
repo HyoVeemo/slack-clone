a3dd144c6143002ed8b1ad8ed83b756b72a63d04
HyoVeemo <dott.goo@gmail.com>
2022-01-08 20:18:13 +0900

    feat(pages): DirectMessage 페이지 인덱스 및 스타일 추가
    - 사용자 데이터, 보낼 사용자 데이터 패칭

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import Workspace from '@layouts/Workspace';
+import React from "react";
+import Workspace from "@layouts/Workspace";
+import { Container, Header } from "./style";
+import gravatar from "gravatar";
+import useSWR from "swr";
+import { IUser } from "@typings/db";
+import fetcher from "@utils/fetcher";
+import { useParams } from "react-router";
+
 
 const DirectMessage = () => {
+  const { workspace, id } = useParams<{ workspace: string, id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher, {
+    dedupingInterval: 2000 // 2초
+  });
+  const { data: myData } = useSWR(`/api/users`, fetcher, {
+    dedupingInterval: 2000 // 2초
+  });
+
+  if (!userData || !myData) {
+    return null;
+  }
+
   return (
-      <div>로그인 하신 것을 축하드려요!</div>
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: "24px", d: "retro" })} alt={userData.nickname} />
+        <span> {userData.nickname}</span>
+      </Header>
+      {/*<ChatList />*/}
+      {/*<ChatBox />*/}
+    </Container>
   );
 };
 
