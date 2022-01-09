8443a56d845b3c5e7fcda32420f18d86bf10405f
HyoVeemo <dott.goo@gmail.com>
2022-01-09 09:40:23 +0900

    feat(components): ChatList 추가

---
+++ components/ChatList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ChatZone, Section } from './style';
+
+const ChatList = () => {
+  return (
+    <ChatZone>
+      <Section>오호</Section>
+    </ChatZone>
+  );
+};
+
+export default ChatList;
