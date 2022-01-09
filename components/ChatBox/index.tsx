e7f717b37df1be99b3fc5ebc3195eb8b7acdfcaf
HyoVeemo <dott.goo@gmail.com>
2022-01-09 09:40:07 +0900

    chore(components): ChatBox Props 추가 및 엔터 시 submit 하도록 수정

--- components/ChatBox/index.tsx
+++ components/ChatBox/index.tsx
@@ -3,19 +3,31 @@ import React, { VFC, useCallback } from 'react';
 
 interface Props {
   chat: string;
+  onSubmitForm: (e: any) => void;
+  onChangeChat: (e: any) => void;
 }
 
-const ChatBox: VFC<Props> = ({ chat }) => {
-  const onSubmitForm = useCallback(() => {}, []);
+// 채널, DM 에서 채팅 했을 때 재사용됨
+// DM 보내기, 혹은 채널 메시지 요청을 하면 재사용이 불가능 하므로 props 로 사용한다.
+const ChatBox: VFC<Props> = ({ chat, onSubmitForm, onChangeChat }) => {
+  const onKeydownChat = useCallback((e) => {
+    if (e.key === 'Enter') {
+      if (!e.shiftKey) {
+        onSubmitForm(e);
+      }
+    }
+  }, []);
 
   return (
     <ChatArea>
       <Form onSubmit={onSubmitForm}>
         <MentionsTextarea>
-          <textarea></textarea>
+          <textarea value={chat} onChange={onChangeChat} onKeyDown={onKeydownChat}>
+            {' '}
+          </textarea>
         </MentionsTextarea>
         <Toolbox>
-          <SendButton>전송</SendButton>
+          <SendButton onSubmit={onSubmitForm}>전송</SendButton>
         </Toolbox>
       </Form>
     </ChatArea>
