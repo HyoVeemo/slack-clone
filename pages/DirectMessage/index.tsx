949ec5c6983a220da74799f32cfd1bdee482f7fa
HyoVeemo <dott.goo@gmail.com>
2022-02-12 15:54:50 +0900

    chore(page): DM 발송 시 서버 요청 전송

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -2,22 +2,45 @@ import React, { useCallback } from 'react';
 import { useParams } from 'react-router';
 import useSWR from 'swr';
 import gravatar from 'gravatar';
+import axios from 'axios';
+import { Container, Header } from './style';
 import fetcher from '@utils/fetcher';
 import useInput from '@hooks/useInput';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
-import { Container, Header } from './style';
+import { IDM } from '@typings/db';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
-  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, fetcher);
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const {
+    data: chatData,
+    mutate: mutateChat,
+    revalidate,
+  } = useSWR<IDM[]>(`/api/workspaces/${workspace}/dms/${id}chats?perPage=${20}&page=${1}`, fetcher);
 
   const [chat, onChangeChat, setChat] = useInput('');
-  const onSubmitForm = useCallback((e) => {
-    e.preventDefault();
-    setChat('');
-  }, []);
+
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(chat);
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            revalidate();
+            // 채팅 등록한 후 채팅 내용 삭제 하기
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat],
+  );
 
   if (!userData || !myData) {
     return null;
