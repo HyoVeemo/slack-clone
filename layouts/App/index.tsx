ca26e66ce427636bbbf991f44b7a32a740149e66
HyoVeemo <dott.goo@gmail.com>
2022-03-20 14:25:06 +0900

    package(): swr devtool 추가

--- layouts/App/index.tsx
+++ layouts/App/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import loadable from '@loadable/component';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import SWRDevtools from '@jjordy/swr-devtools';
 
 const Login = loadable(() => import('@pages/Login'));
 const SignUp = loadable(() => import('@pages/SignUp'));
@@ -8,12 +9,14 @@ const WorkSpace = loadable(() => import('@layouts/Workspace'));
 
 const Index: FC = () => {
   return (
-    <Switch>
-      <Redirect exact path="/" to="/login"></Redirect>
-      <Route path="/login" component={Login}></Route>
-      <Route path="/signup" component={SignUp}></Route>
-      <Route path="/workspace/:workspace" component={WorkSpace}></Route>
-    </Switch>
+    <SWRDevtools>
+      <Switch>
+        <Redirect exact path="/" to="/login"></Redirect>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/signup" component={SignUp}></Route>
+        <Route path="/workspace/:workspace" component={WorkSpace}></Route>
+      </Switch>
+    </SWRDevtools>
   );
 };
 
